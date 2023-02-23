3636cd44d088d5d93e8a326aa783ee81f2d16d70
Giorgi <123399684+giorgid1998@users.noreply.github.com>
2023-02-23 04:53:16 -0800

    Add files via upload

---
+++ lec 9/script.js
@@ -0,0 +1,22 @@
+var income = prompt("შეიყვანეთ თქვენი ხელფასი")
+
+if (income <= 1000) {
+    alert("დაბალი ხელფასი")
+}
+else if (income > 1000 && income <= 1500) {
+    alert("საშუალო ხელფასი")
+}
+else {
+    alert("მაღალი ხელფასი")
+
+}
+
+
+// let v = Math.max(1, 4, 18, 24, 15, 2, 3, 19, 55)
+// console.log(v);
+// v = Math.min(1, 4, 18, 24, 15, 2, 3, 19, 55)
+// console.log(v);
+
+
+
+
